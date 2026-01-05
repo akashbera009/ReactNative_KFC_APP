d00cc642848803aaec612bdf3146649c6b35ba30
Admin <admin@AB.local>
2026-01-05 10:08:44 +0530

    FAQ page animation fix

--- src/screens/ReAnimatedScreen.tsx
+++ src/screens/ReAnimatedScreen.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import AnimatedMeasure from '../components/testing/AnimatedMeasure'
+// import KeyBoardTest from '../components/testing/KeyBoardTest'
 const ReAnimatedScreen = () => {
     return (
         <AnimatedMeasure/>
+        // <KeyBoardTest/>
     )
 }
 export default ReAnimatedScreen
